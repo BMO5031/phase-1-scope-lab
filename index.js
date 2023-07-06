4bcfae19aed221c9b6a97638b9c69652d428276f
VALHALLA-GANGSTER <+>
2023-07-07 01:26:49 +0300

    final commit scope lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Declare customerName variable with 'bob' in global scope
+var customerName = 'bob';
+
+function returnsCustomerName() {
+  return customerName;
+}
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant called leastFavoriteCustomer in global scope and assign it an initial value
+const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer() {
+    // Trying to reassign a constant will result in an error
+    // JavaScript will prevent changing the value of a constant
+    throw new TypeError("Assignment to constant variable.");
+  }
+
+module.exports = {
+  returnsCustomerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+};
